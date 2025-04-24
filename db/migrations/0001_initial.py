fc61682a7c0939e14480eac8a80f3cc4d29173ba
Yaroslav <yaroslav.liubarets@icloud.com>
2025-04-24 15:55:12 +0000

    Solution 1.2

--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255, unique=True)),
-                ('description', models.TextField(null=True)),
+                ('description', models.TextField(null=True, blank=True)),
             ],
         ),
         migrations.CreateModel(
